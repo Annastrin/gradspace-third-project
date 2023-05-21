46b7eb565adcab5e4e83633664e2019d2f4a9863
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2023-05-22 00:25:37 +1200

    Add common types

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+type Product = {
+  id: number
+  category_id: number
+  title: string
+  description: string
+  price: string
+  product_image: string
+  is_active: null
+  created_at: string
+  updated_at: string
+  product_categories: null
+}
+
+export type GetProductsResponse = Product[]
