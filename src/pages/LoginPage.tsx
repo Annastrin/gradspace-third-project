fa71820566ba5f5a3b18a1e38219036315171b3a
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2023-06-02 18:07:21 +1200

    Navigate to home page if user is logged in and opened login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,17 @@
+import React from "react"
+import { Navigate } from "react-router-dom"
 import Box from "@mui/material/Box"
 import Navbar from "../components/Navbar/Navbar"
 import SignIn from "../components/SignIn/SignIn"
+import { AuthContext, AuthContextType } from "../hooks/useAuth"
 
 const LoginPage = () => {
+  const { user } = React.useContext(AuthContext) as AuthContextType
+
+  if (user) {
+    return <Navigate to='/' />
+  }
+
   return (
     <>
       <Navbar />
